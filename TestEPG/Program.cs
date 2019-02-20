f4354ea36f5c9b4710fc03b781387b845d981c5e
Sam <murphysam1997@gmail.com>
2019-02-20 16:59:33 +1100

    Added Test Project for EPG.

---
+++ TestEPG/Program.cs
@@ -0,0 +1,20 @@
+﻿using AustraliaEPG;
+using System;
+
+namespace Epg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine(string.Join<Channel>(",\n\n", 
+                Guide.getGuide(Regions.CANBERRA, AustraliaTimeZone.EASTERN_TIME, AustraliaEPG.DayOfWeek.TUESDAY)));
+            Console.ReadLine();
+        }
+    }
+}
